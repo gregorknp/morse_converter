95df0ce17fd6fb243afbe23a2e02b69ff6f7ca0f
gregorknp <gregoriodmv@gmail.com>
2024-04-27 20:42:22 +0200

    Documentation added decode method added

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 class MorseConverter:
     """
     Class that contains the logis to encode/decode morse code
+
+    Methods
+    -------
+    encode:
+        Method that encodes a human readable string into morse code.
+
+    decode:
+        Method that decodes a morse encoded string to get the original human readable string
+
+    Attributes
+    ----------
+    char_to_morse:
+        Dictionary that associates human readable symbols with it's corresponding morse code
+
     """
+
+    # Dictionary that associates human readable symbols with it's corresponding morse code
     char_to_morse = {  # standard letters
                       'a': "·-", 'b': "-···", 'c': "-·-·", 'd': "-··", 'e': "·", 'f': "··-·", 'g': "--·", 'h': "····",
                       'i': "··", 'j': "·---", 'k': "-·-", 'l': "·-··", 'm': "--", 'n': "-.", 'o': "---", 'p': "·--·",
@@ -20,9 +36,12 @@ class MorseConverter:
         """
         Method that encodes a human readable string into morse code.
 
-        It iterates over each character in the string, uses char_to_morse dictionary to get the corresponding
+        It iterates over each character in the string, uses char_to_morse dictionary to get its corresponding
         morse code and append it to a list. Finally, the encoded string is obtained using join on the list of
-        encoded characters
+        encoded characters.
+
+        Note. If the character being endoded does not exist in the dictionary it is ignored and the final encoded
+        string is not modified.
 
         :param string_to_encode: Input string
         :return: Encoded string
@@ -35,6 +54,26 @@ class MorseConverter:
                 continue
 
         return " ".join(list_encoded_chars)
+
+    def decode(self, morse_string):
+        """
+        Method that decodes a morse encoded string to get the original human readable string
+
+        Iterates over the morse encoded string and for each morse encoded character gets its corresponding human
+        readable character.
+
+        To get the dictionary key from the value, it first gets the position of the value in the dictionary and uses it
+        to index the list of keys.
+
+        :param morse_string: Morse encoded string
+        :return: Original string in human readable format
+        """
+        encoded_list = morse_string.split(" ")
+        decoded_string = []
+        for encoded_char in encoded_list:
+            decoded_string.append(list(self.char_to_morse.keys())[list(self.char_to_morse.values()).index(encoded_char)])
+
+        return "".join(decoded_string)
 
 
 if __name__ == '__main__':
